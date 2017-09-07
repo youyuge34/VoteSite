1f80ffd71a05a6dcca62481e28a22496492e4623
Andy <1197993367@qq.com>
2017-09-07 22:33:59 +0800

    admin后台完成了列表界面

--- poll/admin.py
+++ poll/admin.py
@@ -4,3 +4,26 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ('pub_date', 'question_text')
+    list_display = ['question_text', 'pub_date', 'was_published_recently']
+    fieldsets = (
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    )
+    inlines = (ChoiceInline,)
+    list_filter = ['pub_date', ]
+    search_fields = ['question_text', ]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Choice)
