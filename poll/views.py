69f4692d5b06c87f0ad3c1817072bb4c5fe85879
Andy <1197993367@qq.com>
2017-09-11 17:48:00 +0800

    4、表单处理完成，已可正常使用

--- poll/views.py
+++ poll/views.py
@@ -2,10 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader, RequestContext
-from models import Question
-
+from models import Question, Choice
+from django.core.urlresolvers import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -27,9 +27,24 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'poll/results.html', {'question': question, })
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selectNum = request.POST['choice_post']
+        selectChoice = get_object_or_404(Choice, pk=selectNum)
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'poll/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selectChoice.votes += 1
+        selectChoice.save()
+        # 在增加Choice的得票数之后，代码返回一个 HttpResponseRedirect而不是常用的HttpResponse
+        # 这是约定俗成，为了防止点击返回发生二次post
+        return HttpResponseRedirect(reverse('poll:results', args=(question.id,)))
